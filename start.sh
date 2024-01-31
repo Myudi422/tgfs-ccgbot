085a8eb72c851f7f7645334a6231b24d2be2d274
akuiiki <43848105+Myudi422@users.noreply.github.com>
2024-01-31 18:56:02 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
-git clone "https://github.com/Ayush7445/telegram-forwarder_auto" tgfs
+git clone https://github.com/Ayush7445/telegram-forwarder_auto tgfs
 cd tgfs
+pip3 install -r requirements.txt
 python3 bot.py
